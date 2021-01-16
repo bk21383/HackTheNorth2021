5989ad23045c7e7aff49964528d574e6a2ad0de0
William Chu <wmchu@sfu.ca>
2021-01-16 11:17:00 -0800

    started work on Database

---
+++ src/objects/Database.java
@@ -0,0 +1,45 @@
+package objects;
+
+import java.lang.reflect.Array;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Database implements Iterable<Person> {
+    List<Person> people;
+
+    public Database() {
+        people = new ArrayList<>();
+    }
+
+    public void addPerson(Person person) {
+        people.add(person);
+    }
+
+    public Iterator<Person> iterator() {
+        return people.iterator();
+    }
+
+    public Database findPeopleWithTag(String tag) {
+        Database peopleWithTag = new Database();
+        for(Person person : this) {
+            if(person.hasTag(tag)) {
+                peopleWithTag.addPerson(person);
+            }
+        }
+        return peopleWithTag;
+    }
+
+    public Database findPeopleWIthTags(List<String> tags) {
+        Database peopleWithTag = new Database();
+        for(Person person : this) {
+            for(String tag : tags) {
+                if(person.hasTag(tag)) {
+                    peopleWithTag.addPerson(person);
+                    continue;
+                }
+            }
+        }
+        return peopleWithTag;
+    }
+}
--- src/objects/Person.java
+++ src/objects/Person.java
@@ -46,4 +46,7 @@ public class Person {
         }
     }
 
+    public boolean hasTag(String tag) {
+        return tags.contains(tag);
+    }
 }
